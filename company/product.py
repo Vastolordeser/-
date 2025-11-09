28ee9a9a68ef8d1834c4a4d977b9aabc9638d095
Vastolordeser <sergeyspitzin86@gmail.com>
2025-11-09 12:32:32 +0300

    Update product.py

--- company/product.py
+++ company/product.py
@@ -1,1 +1,9 @@
-
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def __str__(self):
+        return self.name + "(цена: " + str(self.price) + ", количество :" + str(self.quantity) + ")"
+  
